59166315cd75dde7dfbf9a02fde805a09bfea66c
sayitoutloud <m.bina1105@gmail.com>
2025-08-02 13:17:16 +0200

    Update stt.py

--- stt.py
+++ stt.py
@@ -1,40 +1,40 @@
 import speech_recognition as sr
 from lm_studio import LMStudioClient
-from tts import SprachAusgabe
+from tts import SpeechOutput
 
 # Speech To Text
 class STT:
-    def __init__(self, sprache="de-DE"):
+    def __init__(self, lan="en-EN"):
         self.recognizer = sr.Recognizer()
-        self.sprache = sprache
+        self.language = lan
         self.assistent = LMStudioClient()
-        self.tts = SprachAusgabe("de")
+        self.tts = SpeechOutput("en")
 
     def start(self):
         with sr.Microphone() as quelle:
-            print("??? Sprachueberwachung gestartet. Sprich jederzeit...")
+            print("??? Voice monitoring started. You can speak any time...")
             self.recognizer.adjust_for_ambient_noise(quelle)
 
             while True:
                 try:
-                    print("?? ...warte auf Spracheingabe...")
+                    print("?? ...Waiting for voice input...")
                     audio = self.recognizer.listen(quelle)
 
-                    print("?? Verarbeite Sprache...")
-                    text = self.recognizer.recognize_google(audio, language=self.sprache)
+                    print("?? Processing the language...")
+                    text = self.recognizer.recognize_google(audio, language=self.language)
 
-                    print("?? Erkannt:", text)
+                    print("?? Recognized:", text)
 
-                    # ?? An LM Studio schicken
-                    antwort = self.assistent.frage(text)
-                    self.tts.sprechen(antwort)
-                    print("?? Antwort von LM Studio:", antwort)
+                    # ?? Send to LM Studio
+                    answer = self.assistent.question(text)
+                    self.tts.speak(answer)
+                    print("?? Answer of LM Studio:", antwort)
                     print("-" * 50)
 
                 except sr.UnknownValueError:
-                    print("?? Sprache konnte nicht verstanden werden.")
+                    print("?? Speech could not be understood.")
                 except sr.RequestError as e:
-                    print("? Google-Fehler:", e)
+                    print("? Google-Error:", e)
                 except KeyboardInterrupt:
-                    print("\n?? Programm durch Benutzer beendet.")
+                    print("\n?? Program terminated by user.")
                     break
